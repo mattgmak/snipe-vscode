c7dfb217c403b890942f46a446010bcbcabe322f
mgm3592095 <u3592095@connect.hku.hk>
2025-01-26 16:32:55 +0800

    Add basic implementation

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,101 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+
+interface TabItem {
+  label: string;
+  description?: string;
+  tabGroup: vscode.TabGroup;
+  tab: vscode.Tab;
+  shortcut?: string;
+}
+
+// Priority keys similar to snipe.nvim's dictionary
+const PRIORITY_KEYS = "sadflewcmpghio";
+
+function getShortcut(index: number): string {
+  if (index < PRIORITY_KEYS.length) {
+    return PRIORITY_KEYS[index];
+  }
+  return "";
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "snipe-vscode" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "snipe-vscode" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "snipe-vscode.switchTab",
+    async () => {
+      const tabGroups = vscode.window.tabGroups;
+      const allTabs: TabItem[] = [];
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('snipe-vscode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from snipe.vscode!');
-	});
+      // Collect all tabs from all groups
+      tabGroups.all.forEach((group) => {
+        group.tabs.forEach((tab, index) => {
+          const shortcut = getShortcut(index);
+          const label = tab.label;
+          const description =
+            tab.input instanceof vscode.TabInputText
+              ? vscode.workspace.asRelativePath(tab.input.uri)
+              : undefined;
 
-	context.subscriptions.push(disposable);
+          allTabs.push({
+            label: shortcut ? `${shortcut}: ${label}` : label,
+            description,
+            tabGroup: group,
+            tab,
+            shortcut,
+          });
+        });
+      });
+
+      // Create quickpick with keyboard shortcuts
+      const quickPick = vscode.window.createQuickPick<TabItem>();
+      quickPick.items = allTabs;
+      quickPick.placeholder = "Select tab to switch to...";
+      quickPick.matchOnDescription = true;
+
+      // Handle keyboard shortcuts
+      quickPick.onDidChangeValue((value) => {
+        const shortcutTab = allTabs.find(
+          (tab) => tab.shortcut === value.toLowerCase()
+        );
+        if (shortcutTab) {
+          quickPick.hide();
+          if (shortcutTab.tab.input instanceof vscode.TabInputText) {
+            vscode.window.showTextDocument(shortcutTab.tab.input.uri, {
+              preview: false,
+            });
+          }
+        }
+      });
+
+      // Handle normal selection
+      quickPick.onDidAccept(async () => {
+        const selectedTab = quickPick.selectedItems[0];
+        if (
+          selectedTab &&
+          selectedTab.tab.input instanceof vscode.TabInputText
+        ) {
+          await vscode.window.showTextDocument(selectedTab.tab.input.uri, {
+            preview: false,
+          });
+        }
+        quickPick.hide();
+      });
+
+      quickPick.show();
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
