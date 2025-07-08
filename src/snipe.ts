c7a0bd22f1113ddf59492311edbbc9fbc521e461
mattgmak <u3592095@connect.hku.hk>
2025-07-08 14:27:57 +0800

    Add special character commands

--- src/snipe.ts
+++ src/snipe.ts
@@ -1,4 +1,3 @@
-import path from "path";
 import * as vscode from "vscode";
 
 interface TabItem extends vscode.QuickPickItem {
@@ -8,7 +7,8 @@ interface TabItem extends vscode.QuickPickItem {
 }
 
 // Priority keys similar to snipe.nvim's dictionary
-const PRIORITY_KEYS = "sadflewcmpghio";
+// const PRIORITY_KEYS = "sadflewcmpghio";
+const PRIORITY_KEYS = "fjdksla;vmc,x.z/ghtybnrueiwoqp";
 
 function getShortcut(index: number): string {
   if (index < PRIORITY_KEYS.length) {
@@ -33,25 +33,25 @@ function formatLabel(label: string, shortcut: string): string {
     : shortcut.toLowerCase();
 
   // Using ANSI codes for colored keyboard-style shortcut formatting
-  return `[${formattedShortcut}] ${label}`;
+  return `${formattedShortcut} | ${label}`;
 }
 
 // This method is called when your extension is activated
 export function activate(context: vscode.ExtensionContext) {
-  const allLanguageContributes = vscode.extensions.all
-    .map((ext) => {
-      if (ext.packageJSON.contributes?.languages) {
-        return ext.packageJSON.contributes.languages;
-      }
-      return null;
-    })
-    .flat()
-    .filter((language) => language?.id && Array.isArray(language.extensions))
-    .map((language) => {
-      return [language.id, language.extensions];
-    });
-  const allLanguageAssociations = Object.fromEntries(allLanguageContributes);
-  console.log(allLanguageAssociations);
+  // const allLanguageContributes = vscode.extensions.all
+  //   .map((ext) => {
+  //     if (ext.packageJSON.contributes?.languages) {
+  //       return ext.packageJSON.contributes.languages;
+  //     }
+  //     return null;
+  //   })
+  //   .flat()
+  //   .filter((language) => language?.id && Array.isArray(language.extensions))
+  //   .map((language) => {
+  //     return [language.id, language.extensions];
+  //   });
+  // const allLanguageAssociations = Object.fromEntries(allLanguageContributes);
+  // console.log(allLanguageAssociations);
   let disposable = vscode.commands.registerCommand(
     "snipe-vscode.switchTab",
     async () => {
@@ -64,29 +64,29 @@ export function activate(context: vscode.ExtensionContext) {
         .getConfiguration("snipe-vscode")
         .get("showCurrentTab", false);
       const activeTab = vscode.window.tabGroups.activeTabGroup.activeTab;
-      const config = vscode.workspace.getConfiguration();
-      const iconTheme = config.get("workbench.iconTheme");
-      let extId = "";
-      let extJSON = {};
-      for (const ext of vscode.extensions.all) {
-        if (ext.packageJSON.contributes?.iconThemes) {
-          const id = ext.packageJSON.contributes.iconThemes[0].id;
-          if (id === iconTheme) {
-            extId = ext.id;
-            extJSON = ext.packageJSON;
-            const iconThemeId = ext.packageJSON.contributes.iconThemes[0].id;
-            const themePath = path.join(
-              ext.extensionPath,
-              ext.packageJSON.contributes.iconThemes[0].path
-            );
-            const contributes = ext.packageJSON.contributes;
-            console.log(contributes);
-            console.log(iconThemeId);
-            console.log(themePath);
-            break;
-          }
-        }
-      }
+      // const config = vscode.workspace.getConfiguration();
+      // const iconTheme = config.get("workbench.iconTheme");
+      // let extId = "";
+      // let extJSON = {};
+      // for (const ext of vscode.extensions.all) {
+      //   if (ext.packageJSON.contributes?.iconThemes) {
+      //     const id = ext.packageJSON.contributes.iconThemes[0].id;
+      //     if (id === iconTheme) {
+      //       extId = ext.id;
+      //       extJSON = ext.packageJSON;
+      //       const iconThemeId = ext.packageJSON.contributes.iconThemes[0].id;
+      //       const themePath = path.join(
+      //         ext.extensionPath,
+      //         ext.packageJSON.contributes.iconThemes[0].path
+      //       );
+      //       const contributes = ext.packageJSON.contributes;
+      //       console.log(contributes);
+      //       console.log(iconThemeId);
+      //       console.log(themePath);
+      //       break;
+      //     }
+      //   }
+      // }
 
       // Collect all tabs from all groups with dividers
       tabGroups.all.forEach((group, groupIndex) => {
@@ -143,6 +143,19 @@ export function activate(context: vscode.ExtensionContext) {
 
       // Handle alias shortcuts
       quickPick.onDidChangeValue((value) => {
+        // Check if typed character matches any special character command
+        const specialCharacterCommands = vscode.workspace
+          .getConfiguration("snipe-vscode")
+          .get<Record<string, string>>("specialCharacterCommands", {
+            " ": "workbench.action.quickOpen",
+          });
+
+        if (specialCharacterCommands[value]) {
+          quickPick.hide();
+          vscode.commands.executeCommand(specialCharacterCommands[value]);
+          return;
+        }
+
         quickPick.hide();
         const shortcutTab = allTabs.find(
           (tab) =>
